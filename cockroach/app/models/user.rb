823556e79ec77b251890faf2e64f0fa72c9906f7
Alexander Tupavov <tauinbox@gmail.com>
2016-07-13 10:00:40 +0300

    add some user validations

--- cockroach/app/models/user.rb
+++ cockroach/app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
 
-  validates :email, presence: true
+  validates :email, :password, presence: true
   validates :password, confirmation: true
+
 end
