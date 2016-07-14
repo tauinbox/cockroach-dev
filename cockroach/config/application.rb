7e0f202ad8553446ee5db4b8af49364059f9f048
Alexander Tupavov <tauinbox@gmail.com>
2016-07-14 14:50:31 +0300

    override field_error_proc to solve problem with broken markup

--- cockroach/config/application.rb
+++ cockroach/config/application.rb
@@ -11,5 +11,12 @@ module Cockroach
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # !!! Here I'm overriding the field_error_proc to solve the problem when markup breaks
+    # http://stackoverflow.com/questions/5267998/rails-3-field-with-errors-wrapper-changes-the-page-appearance-how-to-avoid-t
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+      html_tag
+    }
+
   end
 end
