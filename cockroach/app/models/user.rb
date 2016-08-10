24e12c6c80e7a74916f550bf54b8d251b13b17d6
Alexander Tupavov <tauinbox@gmail.com>
2016-08-10 09:09:20 +0300

    add :timeoutable option to User model

--- cockroach/app/models/user.rb
+++ cockroach/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable
+         :recoverable, :rememberable, :trackable, :timeoutable
 
   validates :email, :password, presence: true
   validates :password, confirmation: true, length: { in: 8..20 }
